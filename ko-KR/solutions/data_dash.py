570116e1614592f308c0d1d5d90124e88e853ddd
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2022-01-12 14:29:06 +0000

    New translations data_dash.py (Korean)

--- ko-KR/solutions/data_dash.py
+++ ko-KR/solutions/data_dash.py
@@ -3,25 +3,25 @@ from time import sleep
 from datetime import datetime, timedelta
 import requests
 
-no2_motor = Motor('A') #set up slider motor
-no2_motor.run_to_position(0,100) # reset slider position
-pm25_motor = Motor('B') #set up gauge motor
-pm25_motor.run_to_position(0,100) # reset gauge position
+no2_motor = Motor('A') # 슬라이드 모터 설정
+no2_motor.run_to_position(0,100) # 슬라이드 포지션 초기화
+pm25_motor = Motor('B') #게이지 모터 설정
+pm25_motor.run_to_position(0,100) # 게이지 포지션 초기화
 
-no2_min_value = 0 # the lowest NO2 reading you think you will get (This should hopefully be around 0)
-no2_max_value = 60 #the highest NO2 reading you think you will get 
-no2_min_angle = 175 #miniumum motor travel
-no2_max_angle = -175 #maximum motor travel
+no2_min_value = 0 # 예상되는 가장 낮은 NO2 수치 (이 값은 0이어야 함)
+no2_max_value = 60 #예상되는 가장 높은 NO2 수치 
+no2_min_angle = 175 #최소 모터 앵글
+no2_max_angle = -175 #최대 모터 앵글
 
 
-pm25_min_value = 0  # the lowest NO2 reading you think you will get (This should hopefully be around 0)
-pm25_max_value = 100 #the highest pm25 reading you think you will get 
-pm25_min_angle = 175 #miniumum motor travel
-pm25_max_angle = -175 #maximum motor travel
+pm25_min_value = 0 # 예상되는 가장 낮은 PM25 수치 (이 값은 0이어야 함)
+pm25_max_value = 100 #예상되는 가장 높은 PM25 수치 
+pm25_min_angle = 175 #최소 모터 앵글
+pm25_max_angle = -175 #최대 모터 앵글
 
 base_url = "https://docs.openaq.org/v2/measurements"
 
-payload = { #create a dictionary for the API request
+payload = { #API request 를 위한 딕셔너리 생성
     'date_from':'',
     'date_to':'',
     'location_id':'2480',
@@ -32,7 +32,7 @@ payload = { #create a dictionary for the API request
     'offset':'0',
 }
 
-pollution = { #create a dictionary for the pollution readings
+pollution = { #오염 수치에 대한 딕셔너리 생성
     'no2' : 0,
     'pm25': 0,
     }
@@ -53,19 +53,19 @@ def check_air():
     data = response.json()
         
     for reading in data['results']:
-        if reading['parameter'] == 'no2': # This will depend upon what pollutant you are measuring
+        if reading['parameter'] == 'no2': # 측정하려는 오염수치에 따라 달라짐
             pollution['no2'] = reading['value']
-        if reading['parameter'] == 'pm25': # This will depend upon what pollutant you are measuring
+        if reading['parameter'] == 'pm25': # 측정하려는 오염수치에 따라 달라짐
             pollution['pm25'] = reading['value']
 
     output_results()   
     sleep(1)
 
 def remap(min_value, max_value, min_angle, max_angle, sensor_data):
-    value_range = (max_value - min_value)        # work out how wide your value range is
-    motor_range = (max_angle - min_angle)        # work out how wide your motor range is
-    mapped = (((sensor_data - min_value) * motor_range) / value_range) + min_angle        # stretch your value range across your motor range
-    return int(mapped)        # give back a number that shows the value as an angle on the motor
+    value_range = (max_value - min_value)        # value range를 알아내기 위한 코드
+    motor_range = (max_angle - min_angle)        # motor range를 알아내기 위한 코드
+    mapped = (((sensor_data - min_value) * motor_range) / value_range) + min_angle        # 모터 범위에서 값 범위 확장
+    return int(mapped)        # 값을 모터의 앵글로 표시하는 숫자 반환
    
 def output_results():
     print(f"NO2 = {pollution['no2']}")
@@ -85,4 +85,4 @@ def output_results():
 
 while True:
     check_air()
-    sleep(3600)              # wait an hour before checking again (make this smaller for testing purposes)
+    sleep(3600)              # 1시간 대기 이후 다시 확인 (테스트 시 이 값을 작게 조정하세요.)
