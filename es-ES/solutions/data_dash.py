bf95a97d6462a7584e3452346e8adba6d329e028
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2022-01-18 13:00:43 +0000

    New translations data_dash.py (Spanish)

--- es-ES/solutions/data_dash.py
+++ es-ES/solutions/data_dash.py
@@ -3,25 +3,25 @@ from time import sleep
 from datetime import datetime, timedelta
 import requests
 
-no2_motor = Motor('A') #set up slider motor
-no2_motor.run_to_position(0,100) # reset slider position
-pm25_motor = Motor('B') #set up gauge motor
-pm25_motor.run_to_position(0,100) # reset gauge position
+no2_motor = Motor('A') # configura motor deslizante
+no2_motor.run_to_position (0,100) # restablece la posición del control deslizante
+pm25_motor = Motor ('B') # configura el motor del medidor
+pm25_motor.run_to_position (0,100) # restablece la posición del medidor
 
-no2_min_value = 0 # the lowest NO2 reading you think you will get (This should hopefully be around 0)
-no2_max_value = 60 #the highest NO2 reading you think you will get 
-no2_min_angle = 175 #miniumum motor travel
-no2_max_angle = -175 #maximum motor travel
+no2_min_valor = 0 # la lectura más baja de NO2 que crees que obtendrás (esperemos que sea alrededor de 0)
+no2_max_valor = 60 #la lectura NO2 más alta que crees que obtendrás 
+no2_min_angulo = 175 # recorrido mínimo del motor
+no2_max_angulo = -175 # recorrido máximo del motor
 
 
-pm25_min_value = 0  # the lowest NO2 reading you think you will get (This should hopefully be around 0)
-pm25_max_value = 100 #the highest pm25 reading you think you will get 
-pm25_min_angle = 175 #miniumum motor travel
-pm25_max_angle = -175 #maximum motor travel
+pm25_min_valor = 0 # la lectura de NO2 más baja que crees que obtendrás (con suerte, debería ser alrededor de 0)
+pm25_max_valor = 100 #la lectura de pm25 más alta que crees que obtendrás 
+pm25_min_angulo = 175 # recorrido mínimo del motor
+pm25_max_angulo = -175 # recorrido máximo del motor
 
 base_url = "https://docs.openaq.org/v2/measurements"
 
-payload = { #create a dictionary for the API request
+payload = { #crea un diccionario para la solicitud API
     'date_from':'',
     'date_to':'',
     'location_id':'2480',
@@ -32,57 +32,57 @@ payload = { #create a dictionary for the API request
     'offset':'0',
 }
 
-pollution = { #create a dictionary for the pollution readings
+contaminacion = {#crea un diccionario para las lecturas de contaminación
     'no2' : 0,
     'pm25': 0,
     }
 
-def check_air():
-    now = datetime.now()
+def calidad_aire():
+    ahora = datetime.now()
     delta = datetime.now() - timedelta(days=1)
     
     payload['date_from'] = f'{delta:%Y-%m-%d}T{delta:%H:%M:%S}+00:00'
     payload['date_to'] = f'{now:%Y-%m-%d}T{now:%H:%M:%S}+00:00'
     
-    response = requests.get(base_url, params=payload)
+    respuesta = requests.get(base_url, params=payload)
     
-    if response.status_code != 200:
-        print('no response from server')
+    if respuesta.status_code != 200:
+        print ('no hay respuesta del servidor')
         return
     
-    data = response.json()
+    datos = respuesta.json ()
         
-    for reading in data['results']:
-        if reading['parameter'] == 'no2': # This will depend upon what pollutant you are measuring
-            pollution['no2'] = reading['value']
-        if reading['parameter'] == 'pm25': # This will depend upon what pollutant you are measuring
-            pollution['pm25'] = reading['value']
+    for lectura in datos['results']:
+        if lectura['parameter'] == 'no2': # Esto dependerá de qué contaminante estás midiendo
+            contaminacion['no2'] = lectura['value']
+        if lectura['parameter'] == 'pm25': # Esto dependerá de qué contaminante estás midiendo
+            contaminacion['pm25'] = lectura['value']
 
-    output_results()   
+    mostrar_resultado()   
     sleep(1)
 
-def remap(min_value, max_value, min_angle, max_angle, sensor_data):
-    value_range = (max_value - min_value)        # work out how wide your value range is
-    motor_range = (max_angle - min_angle)        # work out how wide your motor range is
-    mapped = (((sensor_data - min_value) * motor_range) / value_range) + min_angle        # stretch your value range across your motor range
-    return int(mapped)        # give back a number that shows the value as an angle on the motor
+def remap(min_valor, max_valor, min_angulo, max_angulo, sensor_datos):
+    valor_rango = (max_valor - min_valor)        # averigua cuál es tu rango de valores
+    motor_rango = (max_angulo - min_angulo) # averigua qué tan amplio es el rango de tu motor
+    mapeado = (((sensor_datos - min_valor) * motor_rango) / valuor_rango) + min_angulo # estira tu rango de valores a lo largo del rango de tus motores
+    return int(mapeado)        # devuelve un número que muestra el valor como un ángulo en el motor
    
-def output_results():
-    print(f"NO2 = {pollution['no2']}")
-    no2_current_angle = no2_motor.get_aposition()
-    no2_sensor_data = int(pollution['no2'])
-    no2_new_angle = remap(no2_min_value, no2_max_value, no2_min_angle, no2_max_angle, no2_sensor_data)
-    if no2_new_angle > no2_current_angle:
-        no2_motor.run_to_position(no2_new_angle, 100, direction="clockwise")
-    elif no2_new_angle < no2_current_angle:
-        no2_motor.run_to_position(no2_new_angle, 100, direction="anticlockwise")
+def mostrar_resultado():
+    print (f "NO2 = {contaminacion ['no2']}")
+    no2_angulo_actual = no2_motor.get_aposition()
+    no2_sensor_data = int(contaminacion ['no2'])
+    no2_nuevo_angulo = remap (no2_min_valor, no2_max_valor, no2_min_angulo, no2_max_angulo, no2_sensor_datos)
+    if no2_nuevo_angulo > no2_angulo_actual:
+        no2_motor.run_to_position(no2_nuevo_angulo, 100, direction="clockwise")
+    elif no2_nuevo_angulo < no2_angulo_actual:
+        no2_motor.run_to_position(no2_nuevo_angulo, 100, direction="anticlockwise")
     sleep(0.1)
-    pm25_sensor_data = int(pollution['pm25'])
-    print(f"PM2.5 = {pollution['pm25']}")
-    pm25_current_angle = pm25_motor.get_aposition()
-    pm25_new_angle = remap(pm25_min_value, pm25_max_value, pm25_min_angle, pm25_max_angle, pm25_sensor_data)
-    pm25_motor.run_to_position(pm25_new_angle, 100)
+    pm25_sensor_datos = int(contaminacion['pm25'])
+    print(f"PM2.5 = {contaminacion['pm25']}")
+    pm25_angulo_actual = pm25_motor.get_aposition()
+    pm25_nuevo_angulo = remap(pm25_min_valuor, pm25_max_valor, pm25_min_angulo, pm25_max_angulo, pm25_sensor_datos)
+    pm25_motor.run_to_position(pm25_nuevo_angulo, 100)
 
 while True:
-    check_air()
-    sleep(3600)              # wait an hour before checking again (make this smaller for testing purposes)
+    calidad_aire()
+    sleep(3600)              # espera una hora antes de volver a preguntar (haz este número más pequeño para hacer pruebas)
